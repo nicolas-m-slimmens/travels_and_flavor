a54ec9cef7a4589c888e83e24fd952e9fc642b3a
nslimmens <nicolas.slimmens@hotmail.com>
2017-01-29 21:52:12 -0300

    Added youtube to CKEditor

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -55,4 +55,11 @@ Ckeditor.setup do |config|
   # Used when CKEditor CDN enabled
   # By default: "ckeditor/config.js"
   # config.js_config_url = 'ckeditor/config.js'
+  assets_root =  Rails.root.join('app','assets','javascripts')
+  ckeditor_plugins_root = assets_root.join('ckeditor','plugins')
+  %w(openlink sourcedialog).each do |ckeditor_plugin|
+    Ckeditor.assets += Dir[ckeditor_plugins_root.join(ckeditor_plugin, '**', '*.js')].map do |x|
+      x.sub(assets_root.to_path, '').sub(/^\/+/, '')
+    end
+  end
 end
