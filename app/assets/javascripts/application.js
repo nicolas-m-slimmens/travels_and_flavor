6050fb787822ebf8594084b5a8dc8f5b443d27f9
nslimmens <nicolas.slimmens@hotmail.com>
2017-02-26 17:46:44 -0300

    Fix infinite scroll icon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,8 +35,6 @@ $(function() {
         $(window).on('scroll', function() {
             var more_posts_url;
             more_posts_url = $('.pagination .next_page').attr('href');
-            console.log(more_posts_url);
-            console.log($(window).scrollTop() > $(document).height() - $(window).height() - 60);
             if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
                 $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
                 $.getScript(more_posts_url);
